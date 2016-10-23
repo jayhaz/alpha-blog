69ad18bbf7ceebfd5799f4818f986809ad5c83f3
Jack <jack@jwhapps.co>
2016-10-24 12:40:21 +1300

    Add categories model & tests

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -10,4 +10,15 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   assert_template 'category/index'
   assert_match 'Sports', response.body
   end
+  
+  test "invalid category submission results in failure" do
+    get new_category_path
+    assert_template 'category/new'
+    assert_no_difference 'Category.count' do 
+      post category_path, category: {name:" "}
+    end
+    assert_template 'category/new'
+    assert_select "h2.panel-title"
+    assert_select "div.panel-body"
+  end
 end
